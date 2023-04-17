4854b98a97313f9a88adb49169b2d0cd527719c1
goatbob <122817160+goatbob@users.noreply.github.com>
2023-04-17 14:13:33 -0500

    Update connect_to_database.py

--- module_2/connect_to_database.py
+++ module_2/connect_to_database.py
@@ -14,6 +14,59 @@ def create_connection(db):
     return None
 
 
+def create_table(conn, sql_create_table):
+    """ Creates table with give sql statement
+    :param conn: Connection object
+    :param sql_create_table: a SQL CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(sql_create_table)
+    except Error as e:
+        print(e)
+
+
+def create_tables(database):
+
+    sql_create_person_table = """ CREATE TABLE IF NOT EXISTS person (
+                                        id integer PRIMARY KEY,
+                                        firstname text NOT NULL,
+                                        lastname text NOT NULL
+                                    ); """
+
+    sql_create_student_table = """CREATE TABLE IF NOT EXISTS student (
+                                    id integer PRIMARY KEY,
+                                    major text NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text,
+                                    FOREIGN KEY (id) REFERENCES person (id)
+                                );"""
+
+    # create a database connection
+    conn = create_connection(database)
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_person_table)
+        # create tasks table
+        create_table(conn, sql_create_student_table)
+    else:
+        print("Unable to connect to " + str(database))
+
+
+def create_person(conn, person):
+    """Create a new person for table
+    :param conn:
+    :param person:
+    :return: person id
+    """
+    sql = ''' INSERT INTO person(firstname,lastname)
+              VALUES(?,?) '''
+    cur = conn.cursor()  # cursor object
+    cur.execute(sql, person)
+    return cur.lastrowid # returns the row id of the cursor object, the person id
+
+
 def select_all_persons(conn):
     """Query all rows of person table
     :param conn: the connection object
@@ -24,12 +77,52 @@ def select_all_persons(conn):
 
     rows = cur.fetchall()
 
-    return rows # return the rows
+    return rows  # return the rows
+
+def create_student(conn, student):
+    """Create a new person for table
+    :param conn:
+    :param student:
+    :return: student id
+    """
+    sql = ''' INSERT INTO student(id, major, begin_date)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()  # cursor object
+    cur.execute(sql, student)
+    return cur.lastrowid  # returns the row id of the cursor object, the student id
+
+
+def select_all_students(conn):
+    """Query all rows of student table
+    :param conn: the connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM student")
+
+    rows = cur.fetchall()
+
+    return rows  # return the rows
 
 
 if __name__ == '__main__':
+    create_tables("pythonsqlite.db")
+
     conn = create_connection("pythonsqlite.db")
+
     with conn:
-        rows = select_all_persons(conn)
-        for row in rows:
+        person = ('Rob', 'Thomas')
+        person_id = create_person(conn, person)
+
+        student = (person_id, 'Songwriting', '2000-01-01')
+        student_id = create_student(conn, student)
+
+    with conn:
+        persons = select_all_persons(conn)
+        for row in persons:
             print(row)
+
+        students = select_all_students(conn)
+        for row in students:
+            print(row)
+
